cfccd395ea592454b2e66f345aa176a857f86116
qianliliang <qian-ll@crystal-optech.com>
2017-12-21 16:40:12 +0800

    独立tomcat正常运行

---
+++ src/main/java/com/hello/SpringBootStartApplication.java
@@ -0,0 +1,16 @@
+package com.hello.controller;
+
+
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+/**
+ * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+ */
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Application.class);
+    }
+}
